a175cba87db8ff3db2a62cf1c793e3d72f7ed73a
Emilio <emiliomtg@gmail.com>
2022-07-03 21:17:41 +0100

    creation of users roles successful

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import jwt from "jsonwebtoken";
 import config from "../config";
+import Role from "../models/Role";
 
 export const signUp = async (req, res) => {
 
@@ -12,6 +13,14 @@ export const signUp = async (req, res) => {
         password: await User.encryptPassword(password),
     })
 
+    if (roles) {
+        const foundRoles = await Role.find({ name: { $in: roles } });
+        newUser.roles = foundRoles.map(role => role._id);
+    } else {
+        const defaultRole = await Role.findOne({ name: "user" });
+        newUser.roles = [defaultRole._id];
+    }
+
     const savedUser = await newUser.save();
     const token = jwt.sign({ id: savedUser._id }, config.SECRET, { expiresIn: 86400 });
 
@@ -19,12 +28,5 @@ export const signUp = async (req, res) => {
 }
 
 export const signIn = async (req, res) => {
-    const { email, password } = req.body;
-    /*try {
-        const user = await User.findByCredentials(email, password);
-        const token = await user.generateAuthToken();
-        res.json({ user, token });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }*/
+    // sign in user
 }
