e1cca89aa246d6a6402cf383d75410714d50d1af
Emilio <emiliomtg@gmail.com>
2022-07-03 22:36:13 +0100

    signin method finished

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -28,5 +28,15 @@ export const signUp = async (req, res) => {
 }
 
 export const signIn = async (req, res) => {
-    // sign in user
+    
+
+    const userFound = await User.findOne({ email: req.body.email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const matchPassword = await User.comparePassword(req.body.password, userFound.password);
+    if( !matchPassword ) return res.status(401).json({ token: null, message: "Invalid Password" });
+
+    const token = jwt.sign({ id: userFound._id }, config.SECRET, { expiresIn: 86400 });
+    res.status(200).json({ token });
+    
 }
